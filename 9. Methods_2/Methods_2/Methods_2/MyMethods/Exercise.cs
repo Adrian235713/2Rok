71561bf3227db12a0f1e84d77d9d09e6218a7b5a
Adrian Poniatowski <66631454+Adrian235713@users.noreply.github.com>
2022-11-01 17:12:29 +0100

    x
    x

--- 9. Methods_2/Methods_2/Methods_2/MyMethods/Exercise.cs
+++ 9. Methods_2/Methods_2/Methods_2/MyMethods/Exercise.cs
@@ -13,7 +13,7 @@ namespace Methods_2.MyMethods
         {
             if (a > b)
                 return a;
-             return b;
+            return b;
         }
 
         //Overloading
@@ -23,20 +23,86 @@ namespace Methods_2.MyMethods
             return result;
         }
         //zad2.Napisz funkcje co przyjmuje trzy argumenty i zwraca (true lub false) - czy da się zrobić trójkąt.
-        //public boo Exercise2(double a, double b, double c)
-        //{
-
-        //}
-
-
+        public bool Exercise2(double a, double b, double c)
+        {
+            return a + b > c && b + c > a && c + a > b ? true : false;
+        }
+        public bool Exercise2(int a, int b, int c)
+        {
+            return a + b > c && b + c > a && c + a > b ? true : false;
+        }
         //zad3.Napisz funkcję która liczy ze wzoru herona
-
+        public double Exercise3(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt((p * (p - a) * (p - b) * (p - c)) / 2);
+        }
+        public double Exercise3(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt((p * (p - a) * (p - b) * (p - c)) / 2);
+        }
         //zad4.Napisz funkcje co przyjmuje listę i zwraca największy element. 
+        public double Exercise4(List<double> list)
+        {
+            double min = list[0];
+            foreach (double elment in list)
+            {
+                min = (elment < min) ? elment : min;
+            }
+            return min;
+        }
 
+        public int Exercise4(List<int> list)
+        {
+            int min = list[0];
+            foreach (int elment in list)
+            {
+                min = (elment < min) ? elment : min;
+            }
+            return min;
+        }
+        //zad5.Napisz funkcje co powie czy podana liczba jest liczba całkowita;
 
-        //zad5.Napisz funkcje 2 argumenty (tablice i n) i powie ile n elementów znajduje się w tej tablicy.
+        public bool Exercise5(double a)
+        {
+            return a % 1 == 0 ? true : false;
+        }
+
+        public bool Exercise5(int a)
+        {
+            return true;
+        }
 
         //zad6.Napisz funkcję co przekształci tablice na listę.
+
+        public List<double> Exercise6(double[] area) 
+        {
+            List<double> list = new List<double>();
+            foreach(double elment in area)
+            {
+                list.Add(elment);
+            }
+            return list;
+        }
+
+        public List<double> Exercise6(int[] area)
+        {
+            List<double> list = new List<double>();
+            foreach (int elment in area)
+            {
+                list.Add(elment);
+            }
+            return list;
+        }
+
+        public void Exercise6Iteration(List<double> list)
+        {
+            foreach(double elment in list)
+            {
+                Console.Write($"{elment}\t");
+            }
+        }
 
 
         //zad7.Napisz funkcję co generuje generuje tabelę z losowymi wartościami z przedziału k i b
--- 9. Methods_2/Methods_2/Methods_2/Run.cs
+++ 9. Methods_2/Methods_2/Methods_2/Run.cs
@@ -17,7 +17,17 @@ namespace Methods_2
             Console.WriteLine(exercise.Exercise1(20,10));
             Console.WriteLine(exercise.Exercise1(20.2323, 10.32));
             Console.WriteLine("---------------------------------------------");
-
+            Console.WriteLine(exercise.Exercise2(5,23.32,10));
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine(exercise.Exercise3(3,4,5));
+            Console.WriteLine("---------------------------------------------");
+            List<int> list = new List<int> {5,4,-10,100};
+            Console.WriteLine(exercise.Exercise4(list));
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine(exercise.Exercise5(5.43));
+            Console.WriteLine("---------------------------------------------");
+            double[] xxx =  {5,6,7,8,10};
+            exercise.Exercise6Iteration(exercise.Exercise6(xxx));
             Console.ReadKey();
         }
     }
