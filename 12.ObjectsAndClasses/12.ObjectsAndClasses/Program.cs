3aa067e360fb124ae1c2a7f874c3e8d9bc8bb42d
Adrian235713 <66631454+Adrian235713@users.noreply.github.com>
2022-11-16 20:48:42 +0100

    c
    c

---
+++ 12.ObjectsAndClasses/12.ObjectsAndClasses/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.ObjectsAndClasses
+{
+    internal class Program
+    {
+        public class Car1
+        {
+            public double Speed;
+
+            //Overloading konstruktor 
+            //-------------------------------
+            public Car1(double speed)
+            {
+                Speed = speed;
+            }
+
+            public Car1()
+            {
+
+            }
+            //-------------------------------
+            public void CarInf()
+            {
+                Console.WriteLine($"Speed {Speed}");
+            }
+        }
+        //================================================
+        public class Car2
+        {
+            public double speed;
+
+            public Car2(double speed)
+            {
+                Speed = speed;
+            }
+
+            // ? Powala nam na korzystanie z null
+            public double? Speed
+            {
+                get { return speed; }
+                set
+                {
+                    if (value >= 500)
+                    {
+                        value = null;
+                    }
+                    else
+                    {
+                        speed = (double)value;
+                    }
+                }
+            }
+            public void carInf()
+            {
+                Console.WriteLine($"Speed {Speed}");
+            }
+        }
+
+        //================================================
+
+        static void Main(string[] args)
+        {
+            //================================================
+            //Car1
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Clas Car 1 ");
+            Console.WriteLine("-----------------------------");
+            //================================================
+            Car1 aaaCar1 = new Car1();
+            Car1 bbbCar1 = new Car1(99999);
+            aaaCar1.CarInf();
+            bbbCar1.CarInf();
+
+            //================================================
+            //Car2
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Clas Car 2 ");
+            Console.WriteLine("-----------------------------");
+            //================================================
+            Car2 aaaCar2 = new Car2(100);
+            Car2 bbbCar2 = new Car2(9999999);
+            aaaCar2.Speed = 1000;
+            aaaCar2.carInf();
+            bbbCar2.carInf();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 12.ObjectsAndClasses/Person_zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_zadanie
+{
+    internal class Program
+    {
+
+        //Napisz klasę, która tworzy osobę.(id, imie, nazwisko wiek, wzrost, wagę, IQ, lista umiejętności; Wykonaj walidacje danych get i set)
+        //Napisz klasę starzenie.Która modyfikuję tę osobę.
+        //Napisz klasę, życie co będzie obsługiwała n.osób (lista) i pozwoli na modyfikować dane wszystkich użytkowiniów.
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
