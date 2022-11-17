41cf76e70eeabc048fad82adfef96e5948060f68
Adrian235713 <66631454+Adrian235713@users.noreply.github.com>
2022-11-17 16:45:31 +0100

    x
    x

---
+++ 12.ObjectsAndClasses/Person_zadanie/Classes/Person.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_zadanie.Classes
+{
+    internal class Person
+    {
+        //Napisz klasę, która tworzy osobę.(id, imie, nazwisko wiek, wzrost, wagę, IQ, lista umiejętności; Wykonaj walidację  danych get i set)
+        public int Id;
+        public string name;
+        public string lastName;
+        public int age;
+        public double height;
+        public int iq;
+        public new List<string> Skills = new List<string>();
+
+        public Person(int id, string name, string lastName, int age, double height, int iq)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            Height = height;
+            Iq = iq;
+        }
+
+        public Person()
+        {
+
+        }
+
+        // Propertys validation 
+        //----------------------------------------------------------------------------
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (value.Length >= 20 || value.Length < 1)
+                    name = "WRONG INPUT";
+                name = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length >= 20 || value.Length <= 1)
+                    lastName = "WRONG INPUT";
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            { 
+                if(value >= 155 || value <= 0)
+                    age = 0;
+                age = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value >= 350 || value <= 0)
+                    height = 0;
+                height = value;
+            }
+        }
+
+        public int Iq
+        {
+            get { return iq; }
+            set
+            {
+                if(value <= 30 || value >= 350)
+                {
+                    iq = 0;
+                }
+                iq = value;
+            }
+        }
+        //----------------------------------------------------------------------------
+
+        public void Information()
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine($"Id\t{Id}");
+            Console.WriteLine($"Name\t{Name}");
+            Console.WriteLine($"LastName\t{LastName}");
+            Console.WriteLine($"Age\t{Age}");
+            Console.WriteLine($"Height\t{Height}");
+            Console.WriteLine($"Iq\t{Iq}");
+            Console.WriteLine("=======================================");
+        }
+
+
+
+
+    }
+}
--- 12.ObjectsAndClasses/Person_zadanie/Program.cs
+++ 12.ObjectsAndClasses/Person_zadanie/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Person_zadanie.Classes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +12,21 @@ namespace Person_zadanie
     internal class Program
     {
 
-        //Napisz klasę, która tworzy osobę.(id, imie, nazwisko wiek, wzrost, wagę, IQ, lista umiejętności; Wykonaj walidacje danych get i set)
+        //Napisz klasę, która tworzy osobę.(id, imie, nazwisko wiek, wzrost, wagę, IQ, lista umiejętności; Wykonaj walidację  danych get i set)
         //Napisz klasę starzenie.Która modyfikuję tę osobę.
-        //Napisz klasę, życie co będzie obsługiwała n.osób (lista) i pozwoli na modyfikować dane wszystkich użytkowiniów.
-
-
+        //Napisz klasę, życie co będzie obsługiwała n.osób (lista) i pozwoli na modyfikować dane wszystkich użytkowników.
         static void Main(string[] args)
         {
+            Console.WriteLine("TEST");
+            Person TEST = new Person();
+            TEST.Information();
+
+
+
+            string x = "fgdgsdfgdfsgdfsgdfsgdfsgdfsgdfgdfsg";
+            Console.WriteLine(x.Length);
+
+            Console.ReadKey();
         }
     }
 }
